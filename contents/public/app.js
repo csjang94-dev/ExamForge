5a6c469c3a73d82b96dcab8b659a0bbe49ec9b23
csjang94-dev <csjang94@gmail.com>
2025-12-08 12:07:52 +0900

    파일 위치 및 경로 변경 다시 모듈화 진행 예정 / 현재는 public에 통합되어있음

---
+++ contents/public/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+// 정적 파일 제공 (public 폴더)
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mainPage.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
