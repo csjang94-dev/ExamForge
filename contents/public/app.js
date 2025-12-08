a00d014955b1f85a3e76aa4641200b146277203f
csjang94-dev <csjang94@gmail.com>
2025-12-08 20:46:36 +0900

    서버 파일 라우팅 해결 상대경로에서 절대경로로 수정

--- contents/public/app.js
+++ contents/public/app.js
@@ -12,8 +12,33 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'mainPage.html'));
+    res.sendFile(path.join(__dirname, 'mainPage', 'mainPage.html'));
 });
+
+app.get('/sampleQuestions', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sampleQuestions', 'sampleQuestions.html'));
+});
+
+app.get('/wordTest', (req, res) => {
+    res.sendFile(path.join(__dirname, 'wordTest', 'flashcard.html'));
+});
+
+app.get('/examReservation', (req, res) => {
+    res.sendFile(path.join(__dirname, 'examReservation', 'examReservation.html'));
+});
+
+app.get('/discount', (req, res) => {
+    res.sendFile(path.join(__dirname, 'discount', 'discount.html'));
+});
+
+app.get('/customerService', (req, res) => {
+    res.sendFile(path.join(__dirname, 'customerService', 'customerService.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login', 'login.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
