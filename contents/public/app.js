976d33c7986102aff048e0b019a50f293495c1cb
csjang94-dev <csjang94@gmail.com>
2025-12-09 22:36:02 +0900

    기출 문제 사이트 완성 / 초안 5문제 json 파일에서 읽어와 서빙

--- contents/public/app.js
+++ contents/public/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import path from 'path';
+import * as fs from 'node:fs/promises';
+import cors from 'cors'
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,9 +10,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
+app.use(cors()); // 명령어를 입력하면 문제가 해결된다.
+
 // 정적 파일 제공 (public 폴더)
 app.use(express.static(path.join(__dirname)));
 
+// 모든 페이지 호스팅
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'mainPage', 'mainPage.html'));
 });
@@ -39,7 +44,21 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'login', 'login.html'));
 });
 
+app.get('/sampleQuestions/api/questions', async(req, res) => {
+    try{
+        const data = await fs.readFile(path.join(__dirname, 'sampleQuestions', 'sampleQuestions.json'));
+        const questions = JSON.parse(data);
+        res.json(questions);
+    }
+    catch(error){
+        console.error('파일 읽기 오류:', error);
+        res.status(500).send('서버 오류: 문제 파일을 불러오는데 실패했습니다.')
+    }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
---
+++ contents/public/sampleQuestions/sampleQuestions.js
@@ -0,0 +1,29 @@
+async function fetchQuestionsAndRender() {
+    try {
+        // 서버의 API 엔드포인트로 Fetch 요청
+        const response = await fetch('/sampleQuestions/api/questions'); 
+        
+        // 응답을 JSON 형식으로 파싱
+        const questions = await response.json(); 
+
+        const questions_container = document.getElementById('questions');
+        
+        // 데이터를 순회하며 HTML로 만들어 페이지에 추가
+        questions.forEach(item => {
+            const elementPtag = document.createElement('p');
+            
+            elementPtag.innerHTML = `[ 문제 ${item.number} ] <br> ${item.question} <br> <br> A : ${item.A}
+            <br> B : ${item.B} <br> C : ${item.C} <br> D : ${item.D} <br> <br> 정답 : ${item.answer} <br> <br> `;
+            
+            questions_container.appendChild(elementPtag);
+        });
+        
+
+    } catch (error) {
+        console.error('문제를 불러오는 중 오류 발생:', error);
+        questions.textContent = '데이터를 불러오는데 실패했습니다.';
+        
+    }
+}
+
+fetchQuestionsAndRender();
